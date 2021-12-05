db67314190568e16af2d2bf06fd951584a9a1197
meso272 <liujinyang1201@126.com>
2021-12-05 15:35:00 -0800

    111

--- src/trainer.py
+++ src/trainer.py
@@ -255,7 +255,7 @@ if __name__ == "__main__":
     parser.add_argument('--save','-s',type=str,default="ckpts_nstxgpi_tenthdefault")
     parser.add_argument('--save_interval','-sv',type=int,default=5)
     parser.add_argument('--cpu','-c',type=bool,default=False)
-    parser.add_argument('--fast','-f',type=bool,default=True)
+    parser.add_argument('--fast','-f',type=bool,default=False)
 
 
     args = parser.parse_args()
